94858e4a6666095b804642695f84add22c27308f
stefanenache12 <stefanenache12@gmail.com>
2023-09-27 17:21:24 +0200

    Add Project relations

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,14 +5,16 @@
         name: "MainComponent",
         data() {
             return {
-                projects: []
+                projects: [],
+               
             }
         },
         methods: {},
         created(){
             axios.get('http://localhost:8000/api/projects')
                  .then(response => {
-                    this.projects = response.data.results;
+                    this.projects = response.data.results.data;
+                    
                 })
                  .catch(error => {
                     console.error('Error:', error);
@@ -24,12 +26,19 @@
 <template>
     <main>
         <section>
-            <div class="container mt-5">
+            <div class="container mt-5 mb-5">
                 <div class="row text-center">
                     <div class="col" v-for="project in projects">
                         <h3 class="py-">{{ project.title }}</h3>
                         <img :src="project.img_link" alt="" class="w-100">
                         <p>{{ project.description }}</p>
+                        <h4 class="py-2">Technologies</h4>
+                        <span class="badge text-bg-info mx-1" v-for="technology in project.technologies"> 
+                            {{ technology.title }}
+                        </span>
+                        <h6 class="py-2">
+                            Type: {{ project.type.title }}
+                        </h6>
                     </div>
                 </div>
             </div>
